e58ae7a76c8106ff793f221f7857068ec2910388
sk <sk_chang@163.com>
2015-06-28 20:41:04 +0800

    optimize

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var flash = require('connect-flash');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 
+var fs = require('fs');
+var accessLog = fs.createWriteStream('access.log',{flags:'a'});
+var errorLog = fs.createWriteStream('error.log', {flags: 'a'});
 var app = express();
 
 // view engine setup
@@ -24,11 +27,16 @@ app.use(flash());
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+app.use(logger({stream:accessLog}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(function(err,req,res,next){
+  var meta = '[' + new Date() + '] ' + req.url + '\n';
+  errorLog.write(meta + err.stack + '\n');
+  next();
+})
 app.use(session({
   secret:settings.cookieSecret,
   key:settings.db,
